d1e626991251ec574abbb8ed53bb026604952d0a
vvemu-eci <87642431+vvemu-eci@users.noreply.github.com>
2021-07-19 19:09:49 +0800

    variables.tf file

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable network_cidr {
+  default = "192.168.100.0/24"
+}
+
+variable subnet_cidr {
+  default = "192.168.100.0/24"
+}
+
+variable region {
+ default="us-west-2"
+}
+
+variable availability_zone {
+  default = "us-west-2a"
+}
+
+variable ami {
+  default = "ami-0fb83677"
+}
+
+variable flavor {
+  default = "t2.micro"
+}
+
+variable keyname {
+  default = "079740061629"
+}
